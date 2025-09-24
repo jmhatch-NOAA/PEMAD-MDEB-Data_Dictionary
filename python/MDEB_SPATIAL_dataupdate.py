b9f909475042925c717e64567bfb5ad187a189a3
Nicole Mucci <nicole.mucci@noaa.gov>
2025-09-24 14:36:51 -0400

    update to method for setting spatial reference

--- python/MDEB_SPATIAL_dataupdate.py
+++ python/MDEB_SPATIAL_dataupdate.py
@@ -15,7 +15,7 @@ from arcgis.geometry import Geometry
 from shapely.wkt import loads as wkt_loads
 
 # AUTHENTICATE ARCGIS CREDENTIALS
-# using ArcGIS Pro to authenticate, change authentication scheme if necessary
+# Using ArcGIS Pro to authenticate, change authentication scheme if necessary
 gis = GIS("PRO")
 
 # CONNECT TO ORACLE
@@ -36,21 +36,16 @@ connection_string = f"oracle+oracledb://{username}:{password}@{tns_name}"
 engine = create_engine(connection_string)
 connection = engine.connect()
 
-# SET VARIABLES
-# Set the spatial reference (WKID) of dataframes
-# AGOL needs a projected coordinate system to calculate Shape_Length and Shape_Area
-spatial_ref_WKID = 102100 # web mercator is standard for AGOL hosted feature services
-
 # DATA EXTRACTION FROM THE DATABASE
-# query table to get AGOL layer info (layer name, url, and layer id)
+# Query table to get AGOL layer info (layer name, url, and layer id)
 layers_sql_query = f"SELECT * FROM {schema}.{lyr_table}"
 df_layers = pd.read_sql(layers_sql_query, con = connection)
 
-# query table to get field info within tables
+# Query table to get field info within tables
 fields_sql_query = f"SELECT * FROM {schema}.{fld_table}"
 df_fields = pd.read_sql(fields_sql_query, con = connection)
 
-# now extract all spatial tables from oracle
+# Now extract all spatial tables from oracle
 # Create an empty dictionary to hold all dataframes
 dataframes = {}
 
@@ -58,8 +53,8 @@ dataframes = {}
 table_names = df_fields['table_name'].unique()
 print("Getting tables from the database...")
 
-# make a separate pandas dataframe for each spatial table in the database
-# ensure that geometry column is handled correctly (convert to wkt)
+# Make a separate pandas dataframe for each spatial table in the database
+# Ensure that geometry column is handled correctly (convert to wkt)
 with engine.connect() as connection:
     for table_name in table_names:
         try:
@@ -71,7 +66,7 @@ with engine.connect() as connection:
             columns_sql_str = ", ".join(columns)
 
             # Build the final SELECT statement.
-            # manually add the SDO_GEOM conversion for the 'shape' column to every query.
+            # Manually add the SDO_GEOM conversion for the 'shape' column to every query.
             final_columns_str = f"{columns_sql_str}, SDO_UTIL.TO_WKTGEOMETRY(SHAPE) AS SHAPE_WKT"
 
             # Construct the final SQL query
@@ -85,66 +80,71 @@ with engine.connect() as connection:
         except Exception as e:
             print(f" FAILED to load table '{table_name}': {e}")
 
- # make all dataframes and columns in dataframes uppercase
- # this is how everything is named in AGOL
+ # Make all dataframes and columns in dataframes uppercase
+ # This is how everything is named in AGOL
 upper_dataframes = {
     key.upper(): df.rename(columns = str.upper) 
     for key, df in dataframes.items()
 }
 print("Converted all data table names and columns to uppercase.")
 
+
 # DATA UPDATE LOOP
 # Loop through each row in df_layers to update a feature layer on AGOL using pandas dataframe
-# make sure to correctly handle geometry (use shapely and create spatially enabled dataframe)
+# Make sure to correctly handle geometry (use shapely and create spatially enabled dataframe)
+
 for index, layer_row in df_layers.iterrows():
     try:
         item_id = layer_row['file_id']
         rest_url = layer_row['rest_url']
         table_name = layer_row['table_name'] # The key to retrieve the data frame from the dictionary
+        # Get the WKID for the hosted feature service layer, use this to set the spatial ref for the sedf
+        feature_layer_item = gis.content.get(item_id)
+        layer_index = int(rest_url[len(rest_url) - 1])
+        target_layer = feature_layer_item.layers[layer_index]
+        spatial_ref_info = target_layer.properties.spatialReference
+        wkid = spatial_ref_info['wkid']
+        latest_wkid = spatial_ref_info.get('latestWkid', wkid)
         
         print(f"--- Processing Layer: '{table_name}' ---")
 
-        # get DataFrame for this layer
+        # Get DataFrame for this layer
         source_df = upper_dataframes.get(table_name)
         if source_df is None or source_df.empty:
             print(f"  - No data found for table '{table_name}'. Skipping.")
             continue
 
-        # create spatially enabled dataframe
+        # Create spatially enabled dataframe
         print("  - Converting WKT to geometry using Shapely...")
         sedf = source_df.copy()
 
         # For each WKT string, load it with Shapely, get its standard geo_interface,
         # and create an arcgis.geometry.Geometry object from that.
+        # Use AGOL feature layer wkid to set spatial reference
+
         sedf['SHAPE'] = sedf['SHAPE_WKT'].apply(
-            lambda wkt: Geometry({
-                **wkt_loads(wkt).__geo_interface__
-            })
+            lambda wkt: Geometry(
+            wkt_loads(wkt).__geo_interface__,
+            spatial_reference={'wkid': latest_wkid}
+            )
         )
 
-        # drop well known text column
+        # Drop well known text column
         sedf.drop('SHAPE_WKT', axis = 1, inplace = True)
 
-        # project into the projected coordinate system
-        # this is so AGOL can calculate Shape_Length and Shape_Area
-        sedf_proj=sedf.spatial.project(spatial_ref_WKID)
+        # Access the FeatureLayer object from AGOL
+        # Get a count of features
+        feature_count = target_layer.query(return_count_only=True)
+        print(f"Checking layer... Found {feature_count} features.")
 
-        # Get the FeatureLayer object from AGOL
-        feature_layer_item = gis.content.get(item_id)
-        # layer_index = int(rest_url.strip('/').split('/')[-1])
-        layer_index = int(rest_url[len(rest_url) - 1])
-        target_layer = feature_layer_item.layers[layer_index]
-
-        # delete data in layer
-        print("  - Deleting existing features...")
-        delete_result = target_layer.delete_features(where = '1=1')
-
-        # check that data delete was successful
-        if delete_result['deleteResults']:
-             print("Successfully deleted features.")
+        # Delete data in layer
+        if feature_count > 0:
+            print("  - Deleting existing features...")
+            delete_result = target_layer.delete_features(where = '1=1')
+            print("Successfully deleted features.")
+        
         else:
-            print("Failed to delete features. Aborting for this layer.")
-            continue 
+            print("Layer is already empty. No features to delete.")
         
         # Add the new features from Spatially Enabled DataFrame
         print(f"  - Adding {len(sedf)} new features...")
